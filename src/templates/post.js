811ca125e6373990f50f2668f461008aeba89524
J <zzai.91@live.com.sg>
2020-02-23 21:35:52 +0800

    Added netlify comments

--- src/templates/post.js
+++ src/templates/post.js
@@ -41,6 +41,30 @@ const Post = ({ data, location }) => {
                                 dangerouslySetInnerHTML={{ __html: post.html }}
                             />
                         </section>
+                        <section className="post-full-comments">
+                            <form name="contact" method="POST" data-netlify="true">
+                                <p>
+                                    <label>Your Name: <input type="text" name="name" /></label>
+                                </p>
+                                <p>
+                                    <label>Your Email: <input type="email" name="email" /></label>
+                                </p>
+                                <p>
+                                    <label>Your Role: 
+                                        <select name="role[]" multiple>
+                                            <option value="leader">Leader</option>
+                                            <option value="follower">Follower</option>
+                                        </select>
+                                    </label>
+                                </p>
+                                <p>
+                                    <label>Message: <textarea name="message"></textarea></label>
+                                </p>
+                                <p>
+                                    <button type="submit">Send</button>
+                                </p>
+                            </form>
+                        </section>
                     </article>
                 </div>
             </Layout>
